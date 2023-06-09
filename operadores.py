c419f2b4521dba699927bc1fb9b27f7cde26ea77
Krlostks <carlospixel13@gmail.com>
2023-06-09 16:51:58 -0600

    04 operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#OPeradores matematicos
+# + - * %
+# / Division float
+# //Division entera
+# ** potencia
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+#Operadores para strings
+# + para concatenar
+# * para replicar
+print("____" * 4)
+print("Hola "+"Mundo!")
+print("Hola "*3)
