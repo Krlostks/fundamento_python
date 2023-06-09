219c763ff482c8e70159fcdfffaf6680ee448541
Krlostks <carlospixel13@gmail.com>
2023-06-09 16:20:17 -0600

    02 variables

---
+++ main.py
@@ -0,0 +1,9 @@
+#Function print
+print("Hola Mundo")
+print("Soy Alumno Holaaaaaaa!!!!!!!")
+
+print("podemos enviar diferentes tipos de datos")
+print(12)
+print(12.23)
+print(True)
+print([])
---
+++ variables.py
@@ -0,0 +1,13 @@
+name = "Krlos"
+last_name = "GG"
+age = 19
+height = 1.67
+is_student = False
+groups = ["3A", "3B", "3C"]
+
+#Function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
