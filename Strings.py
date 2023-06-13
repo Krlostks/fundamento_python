54597ca0a1e7b25dd6d2d9373da0e225fd40fbac
Krlostks <carlospixel13@gmail.com>
2023-06-13 13:01:39 -0600

    07 strings

---
+++ Strings.py
@@ -0,0 +1,23 @@
+my_String = "Este eS mI sTRinG"
+#longitu de string
+print(len(my_String))
+#Mayusculas
+print(my_String.upper())
+#minusculas
+print(my_String.lower())
+#capitalice primer letra a Mayuscula
+print(my_String.capitalize())
+#Eliminar espacios inicio fin
+print(my_String.strip())
+
+#Indexing and Slicing
+my_string = "hola 3C DSM"
+print (my_string[0])
+print (my_string[6])
+print (my_string[3])
+print (my_string[-1])
+print (my_string[-2])
+
+print (my_string[0:2])
+print (my_string[5:7])
+print (my_string[5:-1])
